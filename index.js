cd8f47bab17059f4631bd33c149c40ed9ba8ae1e
Tom Rudick <tmrudick@gmail.com>
2013-02-27 00:48:16 -0800

    Adding almost empty index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+var fs = require('fs'),
+	jade = require('jade');
+
+function Bourbon(options) {
+	// options:
+	//    - template
+	//    - output
+	//    - update
+
+}
+
+exports = Bourbon;
+
+Bourbon.prototype.jobs = function(dir) {
+
+};
