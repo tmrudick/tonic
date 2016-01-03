9bf7ad2e1541dc3395dbf79453dfb8e89fc1b305
Tom Rudick <tmrudick@gmail.com>
2016-01-02 19:53:39 -0500

    Add use method. Delete templates

--- templates/basic/app.js
+++
@@ -1,6 +0,0 @@
-var Tonic = require('tonic');
-
-var tonic = new Tonic();
-tonic.jobs('job.js');
-
-tonic.start();
--- templates/basic/job.js
+++
@@ -1,6 +0,0 @@
-// Print the time to the console ever 10 seconds
-job('print time', function(done) {
-    console.log(new Date());
-
-    done();
-}).every('10s');
